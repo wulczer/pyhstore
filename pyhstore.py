4ad5b55f4622a412eef300b9457e11e710047b59
Jan Urbański <wulczer@wulczer.org>
2010-12-13 08:40:23 +0100

    Only execute a dummy select to force hstore loading if necessary

--- pyhstore.py
+++ pyhstore.py
@@ -3,7 +3,17 @@ try:
     import plpy
 except ImportError:
     raise RuntimeError("%s can only be used in a PL/Python block" % __name__)
-# execute a dummy select to make sure hstore is loaded in the database
-plpy.execute("select '1=>1'::hstore")
-# import the module functions
-from _pyhstore import parse_hstore, serialize_hstore
+
+try:
+    # import the module functions
+    from _pyhstore import parse_hstore, serialize_hstore
+except ImportError, e:
+    # this might mean hstore is not yet loaded in this session, so try to
+    # force loading it
+    try:
+        plpy.execute("select '1=>1'::hstore")
+    except:
+        # did not work for whatever reason, complain with the original exception
+        raise e
+    # try loading the functions again
+    from _pyhstore import parse_hstore, serialize_hstore
